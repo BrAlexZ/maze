ae0a797f59259a8d4dad4560f4f53a203f1c7e95
BrAlexZ <97255307+BrAlexZ@users.noreply.github.com>
2022-01-26 20:59:59 +0100

    Add files via upload

---
+++ js/js-alert.js
@@ -0,0 +1,14 @@
+function myFunction() {
+Swal.fire({
+  title: "CREDITS", 
+  html: "Credits by Alex.Z", 
+  confirmButtonColor: "purple" ,  
+  confirmButtonText: "Vredu",
+  timerProgressBar: true,
+  timer: 3000,  
+});
+}
+
+
+
+
